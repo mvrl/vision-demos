42503cc199478a72b3f770ed0d77764f23d7737c
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2014-01-06 16:11:17 -0500

    Working on implementing a circle detector. I've run into trouble with converting the frames from the webcam to grayscale in order to use the circle detection functions. I'm looking into the cause of this problem

--- eday2014/faceCV2Test.py
+++ eday2014/faceCV2Test.py
@@ -10,12 +10,22 @@ if __name__ == "__main__":
 
     while(1):
         _,im = c.read()
-
+        gray = im
+        cv.CvtColor(im, gray, cv.CV_BGR2GRAY)
+        #gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        
         faces = face_cascade.detectMultiScale(im, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
         
         for (x,y,w,h) in faces:
             cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),1)
-       
+
+        circles = cv2.HoughCircles(im,cv.CV_HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0,:]:
+            cv2.circle(im,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(im,(i[0],i[1]),2,(0,0,255),3)
+        
         cv2.flip(im, 1, im)
 
         cv2.imshow('e2', im)
