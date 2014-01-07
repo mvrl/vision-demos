ecf6b2c4d64360360447cd731e02631001db001e
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2014-01-07 14:35:33 -0500

    Added in some blurring to the image before circle detection.

--- eday2014/faceCV2Test.py
+++ eday2014/faceCV2Test.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
         #gray = im
         #cv.CvtColor(im, gray, cv.CV_BGR2GRAY)
         gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-        #gray = cv2.medianBlur(gray,5)
+        gray = cv2.medianBlur(gray,7)
         
         faces = face_cascade.detectMultiScale(im, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
-        
-        for (x,y,w,h) in faces:
-            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),1)
 
         circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1.5, 20, param1=200, param2=100, minRadius=0, maxRadius=0)
         #circles = np.uint16(np.around(circles))
-
+        
+        for (x,y,w,h) in faces:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),1)
+    
         try:
             for i in circles[0,:10]:
                 cv2.circle(im,(i[0],i[1]),i[2],(0,255,0),2)
