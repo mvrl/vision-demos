e59b31a809bb5d777daf34a09f045fdb9245843d
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2014-01-07 14:20:26 -0500

    Circle detection is in a more functional state.  Tweaked some parameters to have it work better. It still detects large amounts of circles on a pattern such as a plaid shirt, but on a solid background such as a white board, it doesn't crash.

--- eday2014/faceCV2Test.py
+++ eday2014/faceCV2Test.py
@@ -13,19 +13,23 @@ if __name__ == "__main__":
         #gray = im
         #cv.CvtColor(im, gray, cv.CV_BGR2GRAY)
         gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        #gray = cv2.medianBlur(gray,5)
         
         faces = face_cascade.detectMultiScale(im, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
         
         for (x,y,w,h) in faces:
             cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),1)
 
-        circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 20, param1=100, param2=100, minRadius=0, maxRadius=0)
+        circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1.5, 20, param1=200, param2=100, minRadius=0, maxRadius=0)
         #circles = np.uint16(np.around(circles))
 
-        for i in circles[0,:]:
-            cv2.circle(im,(i[0],i[1]),i[2],(0,255,0),2)
-            cv2.circle(im,(i[0],i[1]),2,(0,0,255),3)
-        
+        try:
+            for i in circles[0,:10]:
+                cv2.circle(im,(i[0],i[1]),i[2],(0,255,0),2)
+                cv2.circle(im,(i[0],i[1]),2,(0,0,255),3)
+        except (ValueError, TypeError):
+            print 'no circles'
+            
         cv2.flip(im, 1, im)
 
         cv2.imshow('e2', im)
